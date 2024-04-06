8650a6c1b87aa991ec31c02fad27ec36ef1790d8
Abhishek Kumar <abhishek.andri05@gmail.com>
2024-04-06 22:43:55 +0530

    done

--- src/pages/Openbox.js
+++ src/pages/Openbox.js
@@ -5,7 +5,6 @@ import Inbox from "../components/Inbox";
 import Mails from "../components/Mails";
 import RightSide from "../components/RightSide";
 import Reply from "../components/Reply";
-import axios from "axios";
 import Delete from "../components/Delete";
 
 function Openbox({light, data}){
@@ -14,7 +13,6 @@ function Openbox({light, data}){
     const [threadId, setThreadId] = useState('')
     const [reply, setReply] = useState(false)
     const [del, setDel] = useState(false)
-    const tok = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoiYWJoaXNoZWsuYW5kcmkwNUBnbWFpbC5jb20iLCJpZCI6MTA0LCJmaXJzdE5hbWUiOiJBYmhpc2hlayIsImxhc3ROYW1lIjoiS3VtYXIifSwiaWF0IjoxNzEyMzgwMTU0LCJleHAiOjE3NDM5MTYxNTR9.KgtKIYgkMlVw5dQDJ008d3FkdrrfXBm5u42aWGG3IyU'
     useEffect(() => {
         const handleKeyPress = async (event) => {
             if (event.key === 'r' || event.key === 'R') {
